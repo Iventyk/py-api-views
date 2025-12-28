c858279f80e717aacf41d3763511ec668152d19f
ivan <ivanventik@gmail.com>
2025-12-28 19:02:04 +0200

    Soullution

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from cinema.views import movie_list, movie_detail
-
-urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-]
+from cinema.views import GenreAPIView, ActorGenericView, CinemaHallViewSet, MovieViewSet
 
 app_name = "cinema"
+
+router = DefaultRouter()
+router.register("cinema-halls", CinemaHallViewSet)
+router.register("movies", MovieViewSet)
+
+urlpatterns = [
+    path("genres/", GenreAPIView.as_view()),
+    path("genres/<int:pk>/", GenreAPIView.as_view()),
+
+    path("actors/", ActorGenericView.as_view()),
+    path("actors/<int:pk>/", ActorGenericView.as_view()),
+
+    path("", include(router.urls)),
+]
